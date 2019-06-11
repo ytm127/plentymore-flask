a61e199900ac677005ea8acb24105b235453b723
Thomas Morita <tym479@gmail.com>
2019-06-11 12:37:36 -0500

    adding stuff for deployment

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 # use a class to keep all the configuraiton information organized 
 
@@ -10,5 +13,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    # loggin to stdout for heroku 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
